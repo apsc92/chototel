609a3b1cc713604ca6bcf4d138d097a7d5819c96
apsc92 <anurag@joshsoftware.com>
2016-08-19 11:56:55 +0530

    Added noty and bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def show_flash_notifications
+    notification_js = ''
+
+    flash.each do |t, msg|
+      if msg.is_a?(String)
+        notification_js << "flashNotification(\"#{msg}\", \"#{t}\");"
+      end
+    end
+
+    notification_js
+  end
 end
