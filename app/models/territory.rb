d104c4dda995faebdc7e5f360574e05b16bba213
apsc92 <anurag@joshsoftware.com>
2016-08-20 17:16:20 +0530

    Done some codecleaup

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -4,13 +4,11 @@ class Territory < ApplicationRecord
 
   validates :name, presence: true
 
-  def self.aggregated_order_targets
-    territory_ids = Territory.ids
-    order_targets = User.where(territory_id: territory_ids).group('users.territory_id').sum(:order_target) 
+  def order_target
+    self.users.sum(:order_target)
   end
 
-  def self.aggregated_orders_booked
-    territory_ids = Territory.ids
-    orders_booked = Contact.where(territory_id: territory_ids).group('contacts.territory_id').sum(:order_booked)  
+  def orders_booked
+    self.contacts.sum(:order_booked)
   end
 end
