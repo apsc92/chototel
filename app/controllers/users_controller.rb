ca502b735a6f216933cbde8f5182533d9c40ac26
apsc92 <anurag@joshsoftware.com>
2016-08-22 23:44:08 +0530

    Code cleanup and issue fixing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,22 @@
 class UsersController < ApplicationController
-  before_filter :load_territory, only: [:index, :create]
-  before_filter :load_user, only: :update
+  before_filter :load_territory, only: [:index, :create, :new]
+  before_filter :load_user, only: [:update, :edit]
 
   def index
     @users = @territory.users
   end
 
   def edit
-    @user = User.find(params[:id].to_i) 
   end
 
   def new
-    @user = User.new(territory_id: params[:territory_id])
+    @user = @territory.users.new
   end
 
   def create
-    @user = User.new(user_params)
-    @user.role = params[:user][:role].downcase
-    @user.territory_id = params[:territory_id].to_i
+    @user = @territory.users.new(user_params)
     if @user.save
-      redirect_to territory_users_path(@user.territory), notice: "User Created Successfully"
+      redirect_to territory_users_path(@territory), notice: "User Created Successfully"
     else
       flash.now[:alert] =  "Unable to update User, Please correct the errors and try again"
       render :new, locals: {territory_id: @territory.id} 
