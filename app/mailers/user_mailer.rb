b1b0a337a0b978ead09cae6f307d898cead5eba6
apsc92 <anurag@joshsoftware.com>
2016-08-31 18:31:03 +0530

    Added sendgrid settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: 'sales@chototel.com'
   def send_report(file_path)
     attachments['user_summary.csv'] = File.read(file_path)
-    admin_emails = ['naqisa@chototel.com', 'Rhea@chototel.com', 'joseph.s@chototel.com']
+    #admin_emails = ['naqisa@chototel.com', 'Rhea@chototel.com', 'joseph.s@chototel.com']
+    admin_emails = ['anurag@joshsoftware.com', 'sameert@joshsoftware.com']
     subject = 'Daily report for ' + Date.today.strftime("%d-%m-%Y")
     mail(to: admin_emails, subject: subject) do |format|
       format.html
